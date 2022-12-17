55bddc3918de72a3bde123f6bc4c7d8ccea1b86b
Вика <vika.yefremova.06@mail.ru>
2022-12-17 18:45:00 +0300

    2

--- git.py
+++ git.py
@@ -1,1 +1,25 @@
-print('hello world!')
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle('#2')
+        self.btn = QPushButton('Push', self)
+        self.btn.clicked.connect(self.click)
+
+    def click(self):
+        self.btn.setText('pushed')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
